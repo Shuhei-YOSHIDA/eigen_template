2b6396b9edb69f8e3a57e0663b73b722a62b2bf1
Shuhei_YOSHIDA <s-yoshida@frontier.hokudai.ac.jp>
2019-11-07 09:19:20 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+eigen_template
+
+## How To Use
+```
+$ cd eigen_template
+$ mkdir build
+$ cd build
+$ cmake ..
+$ make
+```
---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <Eigen/Dense>
+
+using namespace std;
+using namespace Eigen;
+
+int main(int argc, char const* argv[])
+{
+    
+    return 0;
+}
